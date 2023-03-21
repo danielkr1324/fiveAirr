61ec147f464ca7cfb645821e41f27544f22ea775
Daniel Krot <danielkr1324@gmail.com>
2023-03-21 21:27:08 +0200

    added dummy object

--- src/router.js
+++ src/router.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 
 import Home from './views/Home.vue'
 import Chat from './views/Chat.vue'
-import GigIndex from './views/GigIndex.vue'
+import GigExploreList from './views/GigExploreList.vue'
 import ReviewIndex from './views/ReviewIndex.vue'
 import LoginSignup from './views/LoginSignup.vue'
 import UserDetails from './views/UserDetails.vue'
@@ -14,9 +14,9 @@ const routes = [
     component: Home,
   },
   {
-    path: '/gig',
-    name: 'GigIndex',
-    component: GigIndex,
+    path: '/explore',
+    name: 'GigExploreList',
+    component: GigExploreList,
   },
   {
     path: '/review',
