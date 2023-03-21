157426cbbbe64c44e02d5c59a1c0d62edfe94060
Don Nissan <donnynis@gamil.com>
2023-03-21 20:58:32 +0200

    add details cmp

---
+++ src/views/GigDetails.vue
@@ -0,0 +1,37 @@
+<template>
+    <section v-if="gig" class="gig-details">
+        <div class="gig-details__details grid">
+            {{ gig }}
+        </div>
+        <aside class="gig-summary">
+
+        </aside>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'GigDetails',
+    data() {
+        return {
+            gig: {
+                "_id": "i101",
+                "title": "I will design your logo",
+                "price": 12
+            }
+        }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    },
+    created() {
+
+    },
+    components: {
+
+    },
+}
+</script>
