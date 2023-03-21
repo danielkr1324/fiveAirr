e96de2f1d23e99d1281a7e4796eba08359b27034
Daniel Krot <danielkr1324@gmail.com>
2023-03-21 20:53:34 +0200

    added some UI libraries

---
+++ src/cmps/GigExplorePreview.vue
@@ -0,0 +1,31 @@
+<template>
+
+</template>
+
+<script>
+export default {
+name: 'GigExplorePreview',
+props: {gig: Object},
+data() {
+return {
+
+}
+},
+methods: {
+
+},
+computed: {
+
+},
+created() {
+
+},
+components: {
+
+},
+}
+</script>
+
+<style>
+
+</style>
