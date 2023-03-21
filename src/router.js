817fe70912639858d1505107513cbb06536df5ee
Daniel Krot <danielkr1324@gmail.com>
2023-03-21 17:43:25 +0200

    first commit

---
+++ src/router.js
@@ -0,0 +1,47 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+
+import Home from './views/Home.vue'
+import Chat from './views/Chat.vue'
+import GigIndex from './views/GigIndex.vue'
+import ReviewIndex from './views/ReviewIndex.vue'
+import LoginSignup from './views/LoginSignup.vue'
+import UserDetails from './views/UserDetails.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home,
+  },
+  {
+    path: '/gig',
+    name: 'GigIndex',
+    component: GigIndex,
+  },
+  {
+    path: '/review',
+    name: 'ReviewIndex',
+    component: ReviewIndex,
+  },
+  {
+    path: '/chat',
+    name: 'Chat',
+    component: Chat,
+  },
+  {
+    path: '/login',
+    name: 'LoginSignup',
+    component: LoginSignup,
+  },
+  {
+    path: '/user/:id',
+    name: 'UserDetails',
+    component: UserDetails,
+  },
+]
+
+export const router = createRouter({
+  routes,
+  history: createWebHashHistory(),
+  // base: process.env.BASE_URL,
+})
---
+++ src/store/store.js
@@ -0,0 +1,17 @@
+import Vuex from 'vuex'
+
+import { userStore } from './user.store.js'
+import { gigStore } from './gig.store.js'
+import { reviewStore } from './review.store.js'
+
+export const store = Vuex.createStore({
+  strict: true,
+  modules: {
+    userStore,
+    gigStore,
+    reviewStore,
+  },
+  state: {},
+  mutations: {},
+  actions: {},
+})
