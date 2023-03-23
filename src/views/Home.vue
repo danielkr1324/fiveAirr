53e8d1b46e20d5ce6ed86a8967476783b57a63b4
Elad Millar <eladmillar@gmail.com>
2023-03-23 14:27:41 +0200

    *homepage cards and start of selling points

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -28,6 +28,28 @@
       <p>Popular professional services</p>
       <PopularCarousel />
     </div>
+    <section class></section>
+
+    <section class="main-container full">
+      <div class="selling-points">
+        <div class="site-info">
+          <h1>A whole world of freelance talent at your fingertips</h1>
+          <ul>
+            <li v-for="point in getSellingText" :key="point">
+              <section class="flex align-center">
+                <h3>{{ point.title }}</h3>
+              </section>
+              <p>{{ point.text }}</p>
+            </li>
+          </ul>
+        </div>
+        <div class="point-image">
+          <img
+            src="https://res.cloudinary.com/dhsdxj3y3/image/upload/v1670793711/gigs/ostlxosopzlu6ccomezg.webp"
+          />
+        </div>
+      </div>
+    </section>
   </section>
 </template>
 
@@ -43,6 +65,32 @@ export default {
   },
   mounted() {
     window.scrollTo(0, 0);
+  },
+  computed: {
+    getSellingText() {
+      return [
+        {
+          title: "The best for every budget",
+          text:
+            " Find high-quality services at every price point. No hourly rates, just project-based pricing."
+        },
+        {
+          title: "Quality work done quickly",
+          text:
+            " Find the right freelancer to begin working on your project within minutes."
+        },
+        {
+          title: "Protected payments, every time",
+          text:
+            " Always know what you'll pay upfront. Your payment isn't released until you approve the work."
+        },
+        {
+          title: "24/7 support",
+          text:
+            " Questions? Our round-the-clock support team is available to help anytime, anywhere."
+        }
+      ];
+    }
   },
   components: {
     AppHero,
