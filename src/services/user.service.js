1164920cf4561757cbcdd7dd4456e70f4e98bebd
Daniel Krot <danielkr1324@gmail.com>
2023-04-01 21:33:35 +0300

    final touches

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -74,7 +74,6 @@ async function login(userCred) {
   }
 }
 async function signup(userCred) {
-  userCred.score = 10000
   if (!userCred.imgUrl)
     userCred.imgUrl =
       'https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png'
@@ -97,12 +96,29 @@ async function changeScore(by) {
   return user.score
 }
 
+function createEmptyUser() {
+  const user = {
+    fullname: '',
+    username: '',
+    password: '',
+    imgUrl:
+      'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/b0b4c759-ad9c-4425-a9f4-ab89e2fd9837/de8cefl-35c0bc59-59b9-42ab-b19f-5c73828bb78e.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2IwYjRjNzU5LWFkOWMtNDQyNS1hOWY0LWFiODllMmZkOTgzN1wvZGU4Y2VmbC0zNWMwYmM1OS01OWI5LTQyYWItYjE5Zi01YzczODI4YmI3OGUucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.81ixeN9b4cfDmfBlskK9CUyAMDtRhYNU7lfwTI8WI5Q',
+    location: '',
+    joinedAt: Date.now(),
+    description: '',
+    level: null,
+    rate: null,
+    avgResponseTime: 1,
+    lastDelivery: null,
+  }
+  return user
+}
+
 function saveLocalUser(user) {
   user = {
     _id: user._id,
     fullname: user.fullname,
     imgUrl: user.imgUrl,
-    score: user.score,
   }
   sessionStorage.setItem(STORAGE_KEY_LOGGEDIN_USER, JSON.stringify(user))
   return user
