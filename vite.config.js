4452709bad373d59e4ad095675130586c5c8d207
Elad Millar <eladmillar@gmail.com>
2023-04-02 11:08:16 +0300

    *build command

--- vite.config.js
+++ vite.config.js
@@ -9,4 +9,8 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  build: {
+    outDir: '../backend/public',
+    emptyOutDir: true
+  }
 })
