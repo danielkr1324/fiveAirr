bf1df99e8aad41c9bb3b8851f23430bba0f1394f
Daniel Krot <danielkr1324@gmail.com>
2023-04-02 18:02:45 +0300

    added backoffice

---
+++ src/cmps/orderList.vue
@@ -0,0 +1,67 @@
+<template>
+    <section>
+        <div class="table-entity flex">
+            <div class="buyer-col flex column align-center user-col">
+                <img :src="order.buyer.imgUrl" />
+                <p class="regular">{{ order.buyer.username }}</p>
+            </div>
+            <div class="gig-col flex column">
+                <span class="table-span regular">{{ order.gig.name }}</span>
+            </div>
+            <div class="due-on-col flex column">
+                <span class="table-span regular">
+                    {{ new Date(order.createdAt).toLocaleDateString() }}
+                </span>
+            </div>
+            <div class="total-col flex column">
+                <span class="table-span regular">US${{ order.gig.price }}</span>
+            </div>
+            <div @click="toggleSet()" class="status-col flex column">
+                <div class="status flex" :class="className(order.status)">
+                    <span class="regular">{{ order.status }}</span>
+                </div>
+            </div>
+            <div @click.self="toggleSet">
+                <div v-if="setOpen" class="set-status" >
+                    <div class="completed status" @click="changeStatus('Completed')">
+                        Completed
+                    </div>
+                    <div class="in-progress status" @click="changeStatus('In Progress')">
+                        In Progress
+                    </div>
+                    <div class="rejected status" @click="changeStatus('Rejected')">
+                        Rejected
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    props: {
+        order: Object
+    },
+    data() {
+        return {
+            setOpen: false,
+        }
+    },
+    methods: {
+        className(str) {
+            if (str === 'Pending') return 'pending'
+            if (str === 'Completed') return 'completed'
+            if (str === 'In Progress') return 'in-progress'
+            if (str === 'Rejected') return 'rejected'
+        },
+        toggleSet() {
+            this.setOpen = !this.setOpen
+        },
+        changeStatus(status) {
+            this.$emit('change', { status, order: this.order })
+            this.toggleSet()
+        },
+    },
+}
+</script>
--- src/views/SellerDashboard.vue
+++ src/views/SellerDashboard.vue
@@ -1,82 +1,171 @@
 <template>
-    <section class="dashboard main-container full">
-        <section class="dashboard-layout">
-
-            <div class="user-profile flex column">
-                <div class="user-profile flex column">
-                    <img class="user-img" :src="loggedUser.imgUrl" />
-                    <h2> {{ loggedUser.fullname }}</h2>
-                    <hr />
-                    <ul>
-                        <li class="flex space-between">
-                            <RouterLink to="/seller/profile">
-                                Seller Profile
-                            </RouterLink>
-                        </li>
-                    </ul>
+    <section class="dashboard flex">
+        <section class="profile-progress">
+            <div class="profile flex">
+                <div v-if="loggedUser" class="img-container">
+                    <img :src="loggedUser.imgUrl">
+                </div>
+                <div class="user-desc flex">
+                    <div class="profile-item">
+                        <p class="regular">Positive Rating</p>
+                        <p class="bold">100%</p>
+                    </div>
+                    <div class="profile-item">
+                        <p class="regular">Response Time</p>
+                        <p class="bold">1 Hrs.</p>
+                    </div>
                 </div>
             </div>
-
-            <div class="orders">
-                <h2 class="headline">Manage Orders</h2>
-                <div class="table-head">
-                    <h4 class="buyer-col">Buyer</h4>
-                    <h4 class="gig-col">Gig</h4>
-                    <h4 class="due-on-col">Order Date</h4>
-                    <h4 class="total-col">Total</h4>
-                    <h4 class="status-col">Status</h4>
+            <div class="progress">
+                <article class="progress-item">
+                    <div class="progress-txt flex">
+                        <p class="bold">Response Rate</p>
+                        <p v-if="(orders.length > 0)">{{ responseRate }}%</p>
+                    </div>
+                    <el-progress v-if="(orders.length > 0)" :percentage="responseRate" color="#1dbf73" />
+                </article>
+                <article class="progress-item">
+                    <div class="progress-txt flex">
+                        <p class="bold">Orders Completed</p>
+                        <p v-if="(orders.length > 0)">{{ completedOrderPercent }}%</p>
+                    </div>
+                    <el-progress v-if="(orders.length > 0)" :percentage="completedOrderPercent" color="#1dbf73" />
+                </article>
+                <article class="progress-item">
+                    <div class="progress-txt flex">
+                        <p class="bold">Delivered on Time</p>
+                        <p v-if="(orders.length > 0)">{{ deliveredOnTime }}%</p>
+                    </div>
+                    <el-progress v-if="(orders.length > 0)" :percentage="deliveredOnTime" color="#1dbf73" />
+                </article>
+            </div>
+        </section>
+        <section class="seller-orders flex">
+            <div class="income-order-dashboard flex">
+                <div class="dashboard-item">
+                    <span class="light">Annual Revenue</span>
+                    <h3 v-if="(orders.length > 0)">${{ annualIncome }}</h3>
                 </div>
-                <div class="order-table" v-for="order in orders" :order="order" :key="order._id">
-                    <span class="buyer-col"> {{ order.buyer.fullname }} </span>
-                    <span class="gig-col">{{ order.gig.name }}</span>
-                    <span class="due-on-col"></span>
-                    <span class="total-col">{{ order.gig.price }}</span>
-                    <span class="status-col">{{ order.status }}</span>
+                <div class="dashboard-item">
+                    <span class="light">Monthly Revenue</span>
+                    <h3 v-if="(orders.length > 0)">${{ monthIncome }}</h3>
+                </div>
+                <div class="dashboard-item">
+                    <span class="light">Completed Orders </span>
+                    <h3 v-if="(orders.length > 0)">{{ annualOrdersComplete }}</h3>
+                </div>
+                <div class="dashboard-item">
+                    <span class="light">Pending Orders </span>
+                    <h3 v-if="(orders.length > 0)">{{ pendingOrders }}</h3>
                 </div>
             </div>
-
+            <h2 class="headline">Manage Orders</h2>
+            <div class="order-table">
+                <div class="table-head flex">
+                    <div class="buyer-col">
+                        <h4>Buyer</h4>
+                    </div>
+                    <div class="gig-col">
+                        <h4>Gig</h4>
+                    </div>
+                    <div class="due-on-col">
+                        <h4>Order Date</h4>
+                    </div>
+                    <div class="total-col">
+                        <h4>Total</h4>
+                    </div>
+                    <div class="status-col">
+                        <h4>Status</h4>
+                    </div>
+                </div>
+                <order-list  v-for="order in orders" :order="order" :key="order._id"
+                    @change="changeStatus" />
+            </div>
         </section>
     </section>
 </template>
 
 <script>
-
-import { svgService } from "@/services/svg.service.js";
-
-
+import { socketService } from '../services/socket.service'
+import OrderList from '../cmps/orderList.vue'
 export default {
-    name: 'SellerDashboard',
+    name: 'seller-orders',
+    components: {
+        OrderList,
+    },
     data() {
         return {
-            orders: null,
+            selectedOrder: null,
+            deliveredOnTime: 95,
+            responseRate: 95,
             loggedUser: null,
         }
-    },
-    methods: {
-        loadOrders() {
-            this.orders = this.$store.dispatch({ type: "loadOrders" })
-        },
-        getSvg(iconName) {
-            return svgService.getSvg(iconName);
-        }
-        // loadUser() {
-        //     this.user = this.$store.getters.user;
-        // }
-    },
-    computed: {
-        orders() {
-            return this.$store.getters.sellerOrders;
-        },
-        // user() {
-        //     return this.$store.getters.loggedinUser
-        // },
     },
     created() {
         this.loadOrders()
         this.loggedUser = this.$store.getters.loggedinUser
+        window.scrollTo({ top: 0, behavior: 'smooth' })
     },
-    components: {
-
+    methods: {
+        loadOrders() {
+            return this.$store.dispatch({ type: 'loadOrders' })
+        },
+        selectOrder(order) {
+            this.selectedOrder = { ...order }
+            this.toggleSet()
+        },
+        toggleSet() {
+            this.setOpen = !this.setOpen
+        },
+        changeStatus({ status, order }) {
+            this.selectOrder(order)
+            this.selectedOrder.status = status
+            this.$store.dispatch({ type: 'saveOrder', order: this.selectedOrder })
+            socketService.emit('order-change-status', this.selectedOrder.buyer)
+        },
+    },
+    computed: {
+        orders() {
+            return this.$store.getters.sellerOrders
+        },
+        annualIncome() {
+            var income = 0
+            this.orders.forEach(order => {
+                if (order.status === "Completed") { income += order.gig.price }
+            })
+            return income
+        },
+        monthIncome() {
+            var income = 0
+            this.orders.forEach(order => {
+                if (order.status === "Completed") { income += order.gig.price }
+            })
+            return income
+        },
+        pendingOrders() {
+            var pending = 0
+            this.orders.forEach(order => {
+                if (order.status === "Pending") { pending++ }
+            })
+            return pending
+        },
+        annualOrdersComplete() {
+            var complete = 0
+            this.orders.forEach(order => {
+                if (order.status === "Completed") { complete++ }
+            })
+            return complete
+        },
+        monthOrdersComplete() {
+            var complete = 0
+            this.orders.forEach(order => {
+                if (order.status === "Complete") { complete++ }
+            })
+            return complete
+        },
+        completedOrderPercent() {
+            return Math.round(((this.annualOrdersComplete / this.orders.length) * 100))
+        },
     },
 }
 </script>
